31ebb5f3b3ae6879d85c70847bf0e1720c8fa9b7
Alessandra <itsalematos@hotmail.com>
2024-04-26 19:34:31 -0300

    Feat: models

--- legislative_data/models.py
+++ legislative_data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Legislator(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    name = models.CharField(max_length=50)
+    balance = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Bill(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    title = models.CharField(max_length=50)
+    primary_sponsor = models.ForeignKey(Legislator, on_delete=models.CASCADE)
+   
+    def __str__(self) -> str:
+        return self.title
